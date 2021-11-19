46d8737a4709b7b7eafd9fb80b2ae7c8e3e9f950
Dani-l4 <mirash.ru137@mail.ru>
2021-11-19 18:43:56 +0300

    added page product-one w/ aside

--- app/js/main.js
+++ app/js/main.js
@@ -6,20 +6,29 @@ $(function(){
 		slidesToShow: 4,
 		slidesToScroll: 3,
 		infinite: true,
-  });
-	 $(".star_rate").rateYo({
-    starWidth: "12px",
-    readOnly: true,
-    rating: 5,
-  });
+	});
+	$(".star_rate").rateYo({
+		starWidth: "12px",
+		readOnly: true,
+		rating: 5,
+	});
 
-	 $('.menu__btn').on('click', function(){
-	 	$('.menu__list').slideToggle();
-	 });
-	 
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').slideToggle();
+	});
+	
 	$('.header__btn-menu').on('click', function(){
-	 	$('.header__box').toggleClass('active');
-	 });
+		$('.header__box').toggleClass('active');
+	});
+
+	$('.product-one__tabs .tab').on('click', function(event) {
+		var id = $(this).attr('data-id');
+		$('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-one__tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#'+id).addClass('active-tab').fadeIn();
+		return false;
+	});
 
 	var mixer = mixitup('.products__inner-box');
 });
